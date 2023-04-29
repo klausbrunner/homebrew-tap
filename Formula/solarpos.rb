3e3c38348c56f6a03b6b397cd699a2c8660aa38c
jreleaserbot <jreleaser@kordamp.org>
2023-04-29 16:17:14 +0000

    solarpos v0.3.3

--- Formula/solarpos.rb
+++ Formula/solarpos.rb
@@ -1,14 +1,14 @@
-# Generated with JReleaser 1.5.0 at 2023-03-05T20:20:42.77111709Z
+# Generated with JReleaser 1.6.0 at 2023-04-29T16:17:03.360999442Z
 class Solarpos < Formula
   desc "A simple command-line application to calculate topocentric solar coordinates and sunrise/sunset
         times."
   homepage "https://github.com/klausbrunner/solarpos"
-  url "https://github.com/klausbrunner/solarpos/releases/download/v0.2.0/solarpos-0.2.0.zip"
-  version "0.2.0"
-  sha256 "8b3d233eb15cc6d6409214b7b1cfdbee9f61a19b10059eacb8311c5bac943f7c"
+  url "https://github.com/klausbrunner/solarpos/releases/download/v0.3.3/solarpos-0.3.3.zip"
+  version "0.3.3"
+  sha256 "6060f8771b1e957cfd344e8bd63546732ba5a1c74c7e9077e6fd0851d45f2ca3"
   license "MIT"
 
-  depends_on "openjdk@17"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
@@ -17,6 +17,6 @@ class Solarpos < Formula
 
   test do
     output = shell_output("#{bin}/solarpos --version")
-    assert_match "0.2.0", output
+    assert_match "0.3.3", output
   end
 end

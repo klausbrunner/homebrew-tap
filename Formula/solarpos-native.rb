635815750abbcc85388703390330f2a3558c871e
jreleaserbot <jreleaser@kordamp.org>
2025-07-21 07:02:00 +0000

    solarpos-native v0.5.1

--- Formula/solarpos-native.rb
+++ Formula/solarpos-native.rb
@@ -1,12 +1,12 @@
-# Generated with JReleaser 1.18.0 at 2025-05-25T19:50:59.393584198Z
+# Generated with JReleaser 1.19.0 at 2025-07-21T07:01:43.123601111Z
 
 class SolarposNative < Formula
   desc "A simple command-line application to calculate topocentric solar coordinates and sunrise/sunset
         times."
   homepage "https://github.com/klausbrunner/solarpos"
-  url "https://github.com/klausbrunner/solarpos/releases/download/v0.5.0/solarpos-0.5.0-osx-aarch_64.zip"
-  version "0.5.0"
-  sha256 "1febe9e1f650f1238de44ebdffa3e34e5d80540cb857fd95e1613085e8b8ffd1"
+  url "https://github.com/klausbrunner/solarpos/releases/download/v0.5.1/solarpos-0.5.1-osx-aarch_64.zip"
+  version "0.5.1"
+  sha256 "5b32563f3cb516156bc09fd11642228a70a2ac99c2f3769ccdc2eedcd6dbc9d3"
   license "MIT"
 
 
@@ -17,6 +17,6 @@ class SolarposNative < Formula
 
   test do
     output = shell_output("#{bin}/solarpos --version")
-    assert_match "0.5.0", output
+    assert_match "0.5.1", output
   end
 end
